d84df949047f9d20f119aca7776fedb11b6b1f57
Marc <marc.ferraro1@gmail.com>
2021-03-11 11:32:04 -0600

    basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Sample.destroy_all
+User.destroy_all
 
-sample1 = Sample.create(title: "Strange Rain Guitar Ending", date: "2009", note: "has drums in background", audio_data:"./audio/strange_rain_guitar.wav")
+user1 = User.create(name: "Marc", username:"marcus")
+
+sample1 = Sample.create(title: "Strange Rain Guitar Ending", date: "2009", note: "has drums in background", shared: true, user_id: user1.id)
+sample2 = Sample.create(title: "Vocals from Radio Recording", date: "20018", note: "can hear the car in the background", shared: true, user_id: user1.id)
+sample3 = Sample.create(title: "garden concert saxophone", date: "2007", note: "fun and pretty clear", shared: true, user_id: user1.id)
+sample4 = Sample.create(title: "flutes", date: "1994", note: "from old record. has kind of an urgent vibe", shared: true, user_id: user1.id)
+
