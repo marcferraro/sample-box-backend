af3cc0f71ca6493dc1ee208ec8fbfd07b44cc627
Marc <marc.ferraro1@gmail.com>
2021-03-12 13:05:56 -0600

    Sample Update Backend

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -14,13 +14,28 @@ class SamplesController < ApplicationController
         else
             render json: {error: "Sample invalid."}
         end
-        
+    end
+
+    def update
+        # byebug
+        sample = Sample.find_by(id: params[:id])
+        if sample.update(update_params)
+            sample.save
+            render json: sample
+        else
+            render json: {error: "Sample edits invalid."}
+        end
     end
 
     private
 
     def sample_params
         params.permit(:title, :date, :note, :shared, :user_id)
+    end
+
+    def update_params
+        # params.except(:id)
+        params.except(:id).permit(:title, :date, :note, :shared)
     end
 
 end
