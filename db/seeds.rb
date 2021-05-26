af03355cb9849d9c006baeb474108fb74901969d
Marc <marc.ferraro1@gmail.com>
2021-05-25 23:34:33 -0500

    db reset, seeds commented

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ User.destroy_all
 
 user1 = User.create(name: "Marc", username:"marcus")
 
-sample1 = Sample.create(title: "Strange Rain Guitar Ending", date: "2009", note: "has drums in background", shared: true, user_id: user1.id)
-sample2 = Sample.create(title: "Vocals from Radio Recording", date: "20018", note: "can hear the car in the background", shared: true, user_id: user1.id)
-sample3 = Sample.create(title: "garden concert saxophone", date: "2007", note: "fun and pretty clear", shared: true, user_id: user1.id)
-sample4 = Sample.create(title: "flutes", date: "1994", note: "from old record. has kind of an urgent vibe", shared: true, user_id: user1.id)
+# sample1 = Sample.create(title: "Strange Rain Guitar Ending", date: "2009", note: "has drums in background", shared: true, user_id: user1.id)
+# sample2 = Sample.create(title: "Vocals from Radio Recording", date: "20018", note: "can hear the car in the background", shared: true, user_id: user1.id)
+# sample3 = Sample.create(title: "garden concert saxophone", date: "2007", note: "fun and pretty clear", shared: true, user_id: user1.id)
+# sample4 = Sample.create(title: "flutes", date: "1994", note: "from old record. has kind of an urgent vibe", shared: true, user_id: user1.id)
 
